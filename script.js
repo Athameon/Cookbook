cbcd2c87ca5b8813ad10a36ff5f3ef9e0c3194f4
Athameon <Benjamin.Nopper@freenet.de>
2021-05-05 15:45:08 +0200

    Chenged the method to show/hide the recipe details to use opacity instead of display. Added second example recipe into the template. Increased font size and line-height to 18px and 1.3em.

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ function displayHideDetails(id, buttonId) {
   let parent = document.getElementById(id);
   if (parent != null) {
     let details = parent.querySelector('.recipe_details');
-    console.log(details.style.display);
+    console.log(details.style.opacity);
     console.log(buttonId);
-    if (details.style.display === "flex") {
-      details.style.display = "none";
+    if (details.style.opacity > 0) {
+      details.style.opacity = 0;
+      details.style.height = 0;
       document.getElementById(buttonId).textContent = "Display"
     } else {
-      details.style.display = "flex";
+      details.style.opacity = 1;
+      details.style.height = 'auto';
       document.getElementById(buttonId).textContent = "Hide"
     }
   }
